0aedeed377501d1289dc0f055c6803df49da0562
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-15 11:11:05 +0100

    Add methods

--- author.rb
+++ author.rb
@@ -1,8 +1,16 @@
-class Author
+require_relative "item"
+
+class Author < Item
   def initialize(first_name,last_name)
+    super()
     @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
   end
 end
--- label.rb
+++ label.rb
@@ -1,8 +1,16 @@
-class Label
+require_relative "item"
+
+class Label < Item
   def initialize(title,color)
+    super()
     @id = Random.rand(1..1000)
     @title = title
     @color = color
     @items = []
   end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+end
 end
