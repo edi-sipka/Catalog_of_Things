ceb22e4142b4fd5157726adc3acf6126a9f6445b
aamir <aamir.khan2478@gmail.com>
2022-11-17 18:30:18 +0500

    Add Database for music album and genre

--- schema.sql
+++ schema.sql
@@ -11,3 +11,27 @@ CREATE TABLE label (
   color VARCHAR(50)
 );
 
+CREATE TABLE musicalbum (
+  id SERIAL PRIMARY KEY,
+  on_spotify BOOLEAN,
+  publish_date Date
+);
+
+CREATE TABLE genre (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+);
+
+CREATE TABLE item (
+ id SERIAL PRIMARY KEY,
+ genre_id INTEGER,
+ author_id INTEGER,
+ label_id INTEGER,
+ publish_date DATE,
+ archived BOOLEAN,
+
+ FOREIGN KEY(genre_id) REFERENCES genre(id), 
+ FOREIGN KEY(author_id) REFERENCES author(id), 
+ FOREIGN KEY(label_id) REFERENCES label(id), 
+);
+
